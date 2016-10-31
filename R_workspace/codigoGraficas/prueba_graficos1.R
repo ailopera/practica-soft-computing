fd286c14953f02576264f0eaf8265e865204fac4
Pedro del Pozo <Pedro del Pozo>
2016-10-31 14:58:47 +0100

    creo que ya tengo algo respecto a los diagramas de dispersión

--- R_workspace/codigoGraficas/prueba_graficos1.R
+++ R_workspace/codigoGraficas/prueba_graficos1.R
@@ -18,6 +18,7 @@ for(i in 1:length(datos_TabuNormalizados)){
   datos_TabuNormalizados[, i] <- datos_TabuNormalizados[, i]/max(datos_TabuNormalizados[, i])
 }
 
+datos_TabuNormalizados[, "indice"] <- c(1:12)
 
 #x <- c(1:100)
 #random_y <- rnorm(100, mean = 0)
@@ -25,9 +26,22 @@ for(i in 1:length(datos_TabuNormalizados)){
 
 #pruebaGrafico1 <- plot_ly(datos_TabuNormalizados, x = datos_TabuNormalizados$executionTime, y = datos_TabuNormalizados$solution, type = 'scatter', mode = 'lines+markers')
 
-pruebaPlot1 <- plot_ly(datos_TabuNormalizados, name = 'executionTime', x = datos_TabuNormalizados$executionTime, y = datos_TabuNormalizados$solution, type = 'scatter', mode = 'markers')
+#pruebaPlot1 <- plot_ly(datos_TabuNormalizados, name = 'executionTime', x = datos_TabuNormalizados$executionTime, y = datos_TabuNormalizados$solution, type = 'scatter', mode = 'markers')
+
+pruebaPlot1 <- plot_ly(datos_TabuNormalizados, name = 'executionTime', x = datos_TabuNormalizados$executionTime, y = datos_TabuNormalizados$efficacy, type = 'scatter', mode = 'markers', symbol = datos_TabuNormalizados$indice, symbols = c(1:12))
 pruebaPlot1 <- add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$maxIterations, name = 'maxIterations', type = 'scatter', mode = 'markers')
 pruebaPlot1 <- add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$maxTabuCount, name = 'maxIteramaxTabuCounttions', type = 'scatter', mode = 'markers')
 pruebaPlot1 <- add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$maxCandidates, name = 'maxCandidates', type = 'scatter', mode = 'markers')
-pruebaPlot1 <- add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$efficacy, name = 'efficacy', type = 'scatter', mode = 'markers')
-add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$solution, name = 'solution', type = 'scatter', mode = 'markers')
+add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$efficacy, name = 'efficacy', type = 'scatter', mode = 'markers')
+#add_trace(p = pruebaPlot1, data = datos_TabuNormalizados, x = datos_TabuNormalizados$solution, name = 'solution', type = 'scatter', mode = 'markers')
+
+
+
+#pruebaPlot2 <- plot_ly(datos_TabuNormalizados, name = 'fila1', x = datos_TabuNormalizados[1,], y = datos_TabuNormalizados$efficacy, type = 'scatter', mode = 'markers', color = I('black'))
+#pruebaPlot2 <- add_trace(p = pruebaPlot2, data = datos_TabuNormalizados, x = datos_TabuNormalizados[2,], name = 'fila2', type = 'scatter', mode = 'markers')
+#pruebaPlot2 <- add_trace(p = pruebaPlot2, data = datos_TabuNormalizados, x = datos_TabuNormalizados[3, ], name = 'fila3', type = 'scatter', mode = 'markers')
+#add_trace(p = pruebaPlot2, data = datos_TabuNormalizados, x = datos_TabuNormalizados[4, ], name = 'fila4', type = 'scatter', mode = 'markers')
+
+
+
+
